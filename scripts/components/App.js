7b90a76ba3954e00cee21b8a9d70e997f589657e
Anna Zhu <zhuanna93@gmail.com>
2016-04-14 23:29:41 -0700

    Updated <App /> and <Messages /> to be stateless components.

--- scripts/components/App.js
+++ scripts/components/App.js
@@ -1,30 +1,28 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react'
 import Messages from './Messages'
 import InputArea from './InputArea'
 import Banner from './Banner'
 import Channels from './Channels'
 
 const propTypes = {
-  message: PropTypes.object.isRequired,
-};
-
-class App extends Component {
-  render() {
-    const { messageList } = this.props.message;
-    return (
-      <div id='content-wrapper' className='clearfix'>
-        <Banner />
-          <Channels />
-          <Messages messageList={messageList}/>
-        <div id='footer'>
-          <div id='filter-feature'></div>
-          <InputArea dispatch={this.props.dispatch} />
-        </div>
-      </div>
-    );
-  }
+  chat: PropTypes.object.isRequired,
 }
 
-App.propTypes = propTypes;
+const App = (props) => {
+  const { messagesList } = this.props.chat
+  return (
+    <div id='content-wrapper' className='clearfix'>
+      <Banner />
+      <Channels />
+      <Messages messagesList={messagesList}/>
+      <div id='footer'>
+        <div id='filter-feature'></div>
+        <InputArea dispatch={this.props.dispatch} />
+      </div>
+    </div>
+  )
+}
 
-export default App;
+App.propTypes = propTypes
+
+export default App
--- scripts/components/Messages.js
+++ scripts/components/Messages.js
@@ -1,51 +1,19 @@
 import React, { Component } from 'react'
 
-class Messages extends Component {
-  render () {
-    // console.log(this.props)
-    var list = this.props.messageList.map(function (msg, index) {
-      return <span className='message-content'>
+const Messages = (props) => {
+
+  var list = props.messagesList.map(function (msg, index) {
+      return <span className='message-content' key={index}>
         <a className='username' href=''>User1</a>
         <span className='time'>2:16PM</span>
-        <p className='message-body' key={index}>{msg}</p>
+        <p className='message-body'>{msg}</p>
       </span>
-    })
+  })
 
-    return (
-      <div id='messages-container'>
-        {/* <message icon> column */ }
-        <span className='message-content'>
-          <a className='username' href=''>Anna</a><span className='time'>2:16PM</span>
-          <p className='message-body'>Maecenas consectetur, massa vitae interdum suscipit, eros
-          olor dapibus ante, ac scelerisque mi velit vitae urna.
-          Pellentesque dictum tempus est sed auctor. Aliquam at pharetra
-          diam. Pellentesque consectetur eros vel sem sodales, a sodales
-          leo gravida. Aenean metus mauris, suscipit a est porttitor
-          </p>
-        </span>
-        <span className='message-content'>
-          <a className='username' href=''>Haylie</a><span className='time'>3:26PM</span>
-          <p className='message-body'>est sed auctor. Aliquam at pharetra.
-          </p>
-        </span>
-        <span className='message-content'>
-          <a className='username' href=''>Anna</a><span className='time'>2:16PM</span>
-          <p className='message-body'>Maecenas consectetur, massa vitae interdum suscipit, eros
-          olor dapibus ante, ac scelerisque mi velit vitae urna.
-          Pellentesque dictum tempus est sed auctor. Aliquam at pharetra
-          diam. Pellentesque consectetur eros vel sem sodales, a sodales
-          leo gravida. Aenean metus mauris, suscipit a est porttitor
-          </p>
-        </span>
-        <span className='message-content'>
-          <a className='username' href=''>Haylie</a><span className='time'>3:26PM</span>
-          <p className='message-body'>est sed auctor. Aliquam at pharetra.
-          </p>
-        </span>
-        <span>{list}</span>
-      </div>
-    )
-  }
+  return <div id='messages-container'>
+    {/* <message icon> column */ }
+    <span>{list}</span>
+  </div>
 }
 
 export default Messages
