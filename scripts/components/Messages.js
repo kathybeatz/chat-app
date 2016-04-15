3d558e0408458a92327e33e29526dbffd4d92da8
Anna Zhu <zhuanna93@gmail.com>
2016-04-14 19:01:45 -0700

    Updated styles & README.

--- scripts/components/Messages.js
+++ scripts/components/Messages.js
@@ -14,36 +14,37 @@ class Messages extends Component {
     return (
       <div id='messages-container'>
         {/* <message icon> column */ }
+        <span className='message-content'>
+          <a className='username' href=''>Anna</a><span className='time'>2:16PM</span>
+          <p className='message-body'>Maecenas consectetur, massa vitae interdum suscipit, eros
+          olor dapibus ante, ac scelerisque mi velit vitae urna.
+          Pellentesque dictum tempus est sed auctor. Aliquam at pharetra
+          diam. Pellentesque consectetur eros vel sem sodales, a sodales
+          leo gravida. Aenean metus mauris, suscipit a est porttitor
+          </p>
+        </span>
+        <span className='message-content'>
+          <a className='username' href=''>Haylie</a><span className='time'>3:26PM</span>
+          <p className='message-body'>est sed auctor. Aliquam at pharetra.
+          </p>
+        </span>
+        <span className='message-content'>
+          <a className='username' href=''>Anna</a><span className='time'>2:16PM</span>
+          <p className='message-body'>Maecenas consectetur, massa vitae interdum suscipit, eros
+          olor dapibus ante, ac scelerisque mi velit vitae urna.
+          Pellentesque dictum tempus est sed auctor. Aliquam at pharetra
+          diam. Pellentesque consectetur eros vel sem sodales, a sodales
+          leo gravida. Aenean metus mauris, suscipit a est porttitor
+          </p>
+        </span>
+        <span className='message-content'>
+          <a className='username' href=''>Haylie</a><span className='time'>3:26PM</span>
+          <p className='message-body'>est sed auctor. Aliquam at pharetra.
+          </p>
+        </span>
         <span>{list}</span>
-        <span className='message-content'>
-          <a className='username' href=''>Anna</a><span className='time'>2:16PM</span>
-          <p className='message-body'>Maecenas consectetur, massa vitae interdum suscipit, eros
-          olor dapibus ante, ac scelerisque mi velit vitae urna.
-          Pellentesque dictum tempus est sed auctor. Aliquam at pharetra
-          diam. Pellentesque consectetur eros vel sem sodales, a sodales
-          leo gravida. Aenean metus mauris, suscipit a est porttitor
-          </p>
-        </span>
-        <span className='message-content'>
-          <a className='username' href=''>Haylie</a><span className='time'>3:26PM</span>
-          <p className='message-body'>est sed auctor. Aliquam at pharetra.
-          </p>
-        </span>
-        <span className='message-content'>
-          <a className='username' href=''>Anna</a><span className='time'>2:16PM</span>
-          <p className='message-body'>Maecenas consectetur, massa vitae interdum suscipit, eros
-          olor dapibus ante, ac scelerisque mi velit vitae urna.
-          Pellentesque dictum tempus est sed auctor. Aliquam at pharetra
-          diam. Pellentesque consectetur eros vel sem sodales, a sodales
-          leo gravida. Aenean metus mauris, suscipit a est porttitor
-          </p>
-        </span>
-        <span className='message-content'>
-          <a className='username' href=''>Haylie</a><span className='time'>3:26PM</span>
-          <p className='message-body'>est sed auctor. Aliquam at pharetra.
-          </p>
-        </span>
-      </div>)
+      </div>
+    )
   }
 }
 
