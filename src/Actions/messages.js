e882945cf75ed0f370abb80f8e461af18fab39ef
Anna Zhu <zhuanna93@gmail.com>
2016-04-17 18:12:14 -0700

    Updated action creators. * Reading from Firebase and dispatching an action to populate the app state with FB data

--- src/Actions/messages.js
+++ src/Actions/messages.js
@@ -1,20 +1,37 @@
 import Firebase from 'firebase'
 let fb = new Firebase('https://chatapp100.firebaseio.com/messages')
 
-function addMessage (data) {
+function addMessage () {
   return {
-    type: 'ADD_MSG',
-    message: data.text,
-    user: data.user
+    type: 'ADD_MESSAGE'
   }
 }
 
 export function createMessage (data) {
   return dispatch => {
-    dispatch(addMessage(data))
+    dispatch(addMessage())
     fb.push({
       message: data.text,
       user: data.user
     })
+  }
+}
+
+export function fetchMessages (data) {
+  // Called when <ChatArea /> first mounts and when
+  // a new message is added.
+  console.log('fetching')
+  return dispatch => {
+    fb.on('child_added', function (dataSnapshot) {
+      console.log(dataSnapshot.val())
+      dispatch(receiveMessages(dataSnapshot.val()))
+    })
+  }
+}
+
+function receiveMessages (dataSnapshot) {
+  return {
+    type: 'LOAD_MESSAGES',
+    data: dataSnapshot
   }
 }
