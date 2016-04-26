d6d4f6d4ee9c13565556674f473f873d4e16f86a
Anna Zhu <zhuanna93@gmail.com>
2016-04-25 23:30:05 -0700

    Updated <ChatHeader /> styles to display a dropdown option to sign out.

--- src/components/ChatHeader.js
+++ src/components/ChatHeader.js
@@ -1,17 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const ChatHeader = (props) => {
-  return (
-    <div id="chat-header">
-      <div id="user-menu">
-        <span>Hi, {props.user.name}</span>
+class ChatHeader extends Component {
+  constructor () {
+    super()
+    this.handleShowMenu = this.handleShowMenu.bind(this)
+    this.handleSignOut = this.handleSignOut.bind(this)
+    this.state = {
+      style: 'none'
+    }
+  }
+  handleSignOut () {
+    console.log('sign out')
+  }
+  handleShowMenu () {
+    this.setState({ style: ''})
+  }
+  handleHideMenu () {
+    // why can't I bind this func in constructor?
+    this.setState({ style: 'none'})
+  }
+  render () {
+
+    return (
+      <div id='chat-header'>
+        <div id='user-menu'>
+          <button onClick={this.handleShowMenu} onBlur={this.handleHideMenu.bind(this)}>Hi, {this.props.user.name}<div className="arrow-down">&#9660;</div></button>
+          <div id='signout-dropdown' style={{'display': this.state.style}}>
+            <a href='#'onClick={this.handleSignOut}>Sign out</a>
+          </div>
+        </div>
+        <div id='active-channel-name'>
+          <span>{this.props.active.name}</span>
+        </div>
+       {/* Add a drop down of the users in channel */}
       </div>
-      <div id="active-channel-name">
-        <span>{props.active.name}</span>
-      </div>
-     {/* Add a drop down of the users in channel */}
-    </div>
-  )
+    )
+  }
 }
 
 export default ChatHeader
